21bb3669cb13cf375fbd95440b87886de7cea370
Song <217.Songs.SH@gmail.com>
2021-01-27 18:24:46 +0900

    memory

--- lake/DeepLake.py
+++ lake/DeepLake.py
@@ -4,6 +4,32 @@ import random
 import matplotlib.pyplot as plt
 import torch
 from gym.envs.registration import register
+from collections import namedtuple
+
+Transition = namedtuple('Transition',
+                        ('state', 'action', 'next_state', 'reward'))
+
+
+class ReplayMemory(object):
+
+    def __init__(self, capacity):
+        self.capacity = capacity
+        self.memory = []
+        self.position = 0
+
+    def push(self, *args):
+        """transition 저장"""
+        if len(self.memory) < self.capacity:
+            self.memory.append(None)
+        self.memory[self.position] = Transition(*args)
+        self.position = (self.position + 1) % self.capacity
+
+    def sample(self, batch_size):
+        return random.sample(self.memory, batch_size)
+
+    def __len__(self):
+        return len(self.memory)
+
 
 register(
     id='FrozenLake-v3',
@@ -23,8 +49,11 @@ def makemodel():
         torch.nn.ReLU(),
         torch.nn.Linear(10, 4),
     )
+    model = model.cuda()
     return model
 
+
+memory = ReplayMemory(10000)
 
 DQNmodel = makemodel()
 
@@ -37,9 +66,38 @@ rList = []
 eList = []
 dis = .95
 e = 0.1
-num_episodes = 5000
+num_episodes = 10000
 
 wincnt = 0
+
+BATCH_SIZE = 100
+
+
+def optimize_model():
+    if len(memory) < BATCH_SIZE:
+        return
+    transitions = memory.sample(BATCH_SIZE)
+
+    batch = Transition(*zip(*transitions))
+
+    state_batch = torch.cat(batch.state)
+    next_state_batch = torch.cat(batch.next_state)
+
+    state_batch = state_batch.view(BATCH_SIZE, 16)
+    next_state_batch = next_state_batch.view(BATCH_SIZE, 16)
+    Q = DQNmodel(state_batch.cuda())
+    Q1all = DQNmodel(next_state_batch.cuda())
+
+    with torch.no_grad():
+        maxQ1 = float(torch.max(Q1all))
+        targetQ = Q.clone()
+        targetQ[a] = r + dis * maxQ1
+
+    loss = loss_fn(Q, targetQ)
+    optimizer.zero_grad()
+    loss.backward()
+    optimizer.step()
+    print(i, loss.item())
 
 
 for i in range(num_episodes):
@@ -48,11 +106,10 @@ for i in range(num_episodes):
     done = False
     j = 0
     eList.append(e)
-    loss = None
     while not done:
         j += 1
         s_hot = torch.from_numpy(np.identity(16)[s:s + 1][0]).float()
-        allQ = DQNmodel(s_hot)
+        allQ = DQNmodel(s_hot.cuda())
         a = int(torch.argmax(allQ))
         if np.random.rand(1) < e:
             a = env.action_space.sample()
@@ -60,7 +117,6 @@ for i in range(num_episodes):
         s1, r, done, _ = env.step(a)
 
         s1_hot = torch.from_numpy(np.identity(16)[s1:s1 + 1][0]).float()
-        Q1all = DQNmodel(s1_hot)
 
         if r == 0.0:
             r = -0.01
@@ -70,28 +126,19 @@ for i in range(num_episodes):
             r = 1
             wincnt += 1
 
-
-        with torch.no_grad():
-            maxQ1 = float(torch.max(Q1all))
-            targetQ = allQ.clone()
-            targetQ[a] = r + dis * maxQ1
-
-        loss = loss_fn(allQ, targetQ)
-        optimizer.zero_grad()
-        loss.backward()
-        optimizer.step()
+        memory.push(s_hot, a, s1_hot, r)
 
         rAll += r
         s = s1
-
-
 
         if done:
             e = 1. / ((i / 50) + 10)
             break
 
-    if i % 100 == 99:
-        print(i, loss.item())
+        if j > 100:
+            break
+    if i % 50 == 0:
+        optimize_model()
 
     jList.append(j)
     rList.append(rAll)
